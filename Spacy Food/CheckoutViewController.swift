09cbdcbce15ad5db94e2e360455d613619ae508d
dmytro.khl <dmytro.khludkov@vgs.io>
2020-03-19 19:12:59 +0200

    Add checkout view controller.

---
+++ Spacy Food/CheckoutViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CheckoutViewController.swift
+//  Spacy Food
+//
+//  Created by Dima on 19.03.2020.
+//  Copyright © 2020 Very Good Security. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CheckoutViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationItem.rightBarButtonItem?.title = "Back"
+    }
+    
+    @IBAction func payAction(_ sender: Any) {
+        
+    }
+}
