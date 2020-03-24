1dfb83365483ce2c6c909e59a13301f9f1796c51
dmytro.khl <dmytro.khludkov@vgs.io>
2020-03-24 13:18:49 +0200

    Update collect card screen

--- Spacy Food/CollectCreditCardDataViewController.swift
+++ Spacy Food/CollectCreditCardDataViewController.swift
@@ -10,7 +10,6 @@ import Foundation
 import UIKit
 import VGSCollectSDK
 
-
 struct SecuredCardData {
     let cardNumberAlias: String
     let cvcAlias: String
@@ -22,31 +21,47 @@ struct SecuredCardData {
 class CollectCreditCardDataViewController: UIViewController {
 
     @IBOutlet weak var cardDataStackView: UIStackView!
+    @IBOutlet weak var bluredBackground: UIVisualEffectView!
+    @IBOutlet weak var containerViewBottomConstraint: NSLayoutConstraint!
     
     // Init VGS Collector
-    var collector = VGSCollect(id: "tntdw5wjxet", environment: .sandbox)
+    var collector = VGSCollect(id: "tnttftgwu66", environment: .sandbox)
     
-    var scanController: VGSCardIOScanController?
-    
-    // VGS UI Elements
+    // VGS Elements
     var cardNumber = VGSCardTextField()
     var expCardDate = VGSTextField()
     var cvcCardNum = VGSTextField()
-    
+    var scanController: VGSCardIOScanController?
+
     // Native UI Elements
     var cardHolderName = UITextField()
     
+    // Helpers
+    var keyboardVisible = false
+    var maxLevel = 0 as CGFloat
+    var initialTouchPoint = CGPoint.zero
+    let notificationFeedbackGenerator = UINotificationFeedbackGenerator()
+    let impactFeedbackGenerator = UIImpactFeedbackGenerator()
     var onCompletion: ((SecuredCardData) -> Void )?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
+        buildUI()
         setupElementsConfiguration()
-        UITextField.appearance().keyboardAppearance = UIKeyboardAppearance.dark
+        
+        // Helpers
+        addGestureRecognizer()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        cardHolderName.becomeFirstResponder()
     }
     
     // MARK: - Setup UI
-    private func setupUI() {
+    private func buildUI() {
 
         cardDataStackView.addArrangedSubview(cardHolderName)
         cardDataStackView.addArrangedSubview(cardNumber)
@@ -74,7 +89,6 @@ class CollectCreditCardDataViewController: UIViewController {
         cardNumber.padding = padding
         cardNumber.placeholder = "Card Number"
         cardNumber.textAlignment = .natural
-        cardNumber.becomeFirstResponder()
 
         let expDateConfiguration = VGSConfiguration(collector: collector, fieldName: "card_expirationDate")
         expDateConfiguration.isRequiredValidOnly = true
@@ -103,6 +117,7 @@ class CollectCreditCardDataViewController: UIViewController {
         cardHolderName.layer.borderColor = UIColor.lightGray.cgColor
         cardHolderName.layer.cornerRadius = 4
         
+        cardHolderName.autocorrectionType = .no
         cardHolderName.textColor = .white
         cardHolderName.placeholder = "Cardholder Name"
         cardHolderName.font = textFont
@@ -122,32 +137,42 @@ class CollectCreditCardDataViewController: UIViewController {
         scanController?.presentCardScanner(on: self, animated: true, completion: nil)
     }
     
+    
     @IBAction func save(_ sender: Any) {
+        impactFeedbackGenerator.impactOccurred()
+        
+        //check card state attribures
         let cardState = cardNumber.state as? CardState
         let last4 = cardState?.last4 ?? ""
         let brand = cardState?.cardBrand.stringValue() ?? ""
         
         collector.submit(path: "/post") { [weak self](json, error) in
+            
             if let data = json?["json"] as? [String: Any],
                 let cardNumberAlias = data["card_number"] as? String,
                 let cvcAlias = data["card_cvc"]  as? String,
                 let expDataAlias = data["card_expirationDate"]  as? String  {
+                
                 let cardData = SecuredCardData.init(cardNumberAlias: cardNumberAlias, cvcAlias: cvcAlias, expDataAlias: expDataAlias, cardNumberLast4: last4, cardBrand: brand)
                 self?.onCompletion?(cardData)
                 self?.dismiss(animated: true, completion: nil)
+                
+                self?.notificationFeedbackGenerator.notificationOccurred(.success)
             } else {
+
                 if let error = error as? NSError {
                     print(error.description)
                 } else {
                     // data not full
                 }
+                self?.notificationFeedbackGenerator.notificationOccurred(.error)
             }
         }
     }
-    
-    
 }
 
+// MARK: - VGSCardIOScanControllerDelegate
+// Handle Card Scanning Delegate
 extension CollectCreditCardDataViewController: VGSCardIOScanControllerDelegate {
     func textFieldForScannedData(type: CradIODataType) -> VGSTextField? {
         switch type {
@@ -170,3 +195,72 @@ extension CollectCreditCardDataViewController: VGSCardIOScanControllerDelegate {
         scanController?.dismissCardScanner(animated: true, completion: nil)
     }
 }
+
+// MARK: - Helpers
+extension CollectCreditCardDataViewController {
+    
+    func addGestureRecognizer() {
+        let gestureRecognizer = UIPanGestureRecognizer(target: self,
+                                                   action: #selector(panGestureRecognizerHandler(_:)))
+        self.view.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @IBAction func panGestureRecognizerHandler(_ sender: UIPanGestureRecognizer) {
+        let touchPoint = sender.location(in: self.view?.window)
+        switch sender.state {
+        case .began:
+            initialTouchPoint = touchPoint
+        case .changed:
+            if touchPoint.y - initialTouchPoint.y > 0 {
+                changeContainerPosition(touchPoint.y - initialTouchPoint.y)
+            }
+            self.bluredBackground.alpha = 0.8 * initialTouchPoint.y / touchPoint.y
+        case .ended, .cancelled:
+             if touchPoint.y - initialTouchPoint.y > 100 {
+                self.view.endEditing(true)
+                let haptic = UIImpactFeedbackGenerator()
+                haptic.impactOccurred()
+                UIView.animate(withDuration: 0.1, animations: {
+                    self.containerViewBottomConstraint.constant = -100
+                    self.bluredBackground.alpha = 0
+                    self.view.layoutIfNeeded()
+                }) { (bool) in
+                     self.dismiss(animated: false, completion: nil)
+                }
+             } else {
+                //to original
+                containerViewBottomConstraint.constant = maxLevel
+                view.layoutIfNeeded()
+                self.bluredBackground.alpha = 0.8
+            }
+               
+         default:
+            return
+        }
+    }
+    
+    func changeContainerPosition(_ delta: CGFloat) {
+        containerViewBottomConstraint.constant = maxLevel - delta
+        view.layoutIfNeeded()
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        let userInfo = notification.userInfo
+        
+        guard keyboardVisible == false else {
+            return
+        }
+        keyboardVisible = true
+        
+        if let info = userInfo, let kbRect = (info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let height = kbRect.size.height - 40.0
+            self.maxLevel = height
+            self.containerViewBottomConstraint.constant = height
+            UIView.animate(withDuration: 10, delay: 0, options: .curveEaseInOut, animations: {
+                self.bluredBackground.alpha = 0.9
+                self.view.layoutIfNeeded()
+            }, completion: nil)
+        }
+    }
+}
+
