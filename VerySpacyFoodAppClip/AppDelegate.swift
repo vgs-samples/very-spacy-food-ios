6c3b576b79dc99fee10287d11e49dcd175a3870a
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-09-17 10:08:35 +0300

    Add app clip target

---
+++ VerySpacyFoodAppClip/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  VerySpacyFoodAppClip
+//
+//  Copyright © 2021 Very Good Security. All rights reserved.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+}
+
---
+++ VerySpacyFoodAppClip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VerySpacyFoodAppClip
+//
+//  Copyright © 2021 Very Good Security. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
