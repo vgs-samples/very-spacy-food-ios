167d7e14455294d1a21a45ca3029240abe54610a
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-09-17 10:05:59 +0300

    Update pods

---
+++ Pods/Target Support Files/Pods-Very Spacy Food/Pods-Very Spacy Food-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Very_Spacy_Food : NSObject
+@end
+@implementation PodsDummy_Pods_Very_Spacy_Food
+@end
---
+++ Pods/Target Support Files/Pods-Very Spacy FoodTests/Pods-Very Spacy FoodTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Very_Spacy_FoodTests : NSObject
+@end
+@implementation PodsDummy_Pods_Very_Spacy_FoodTests
+@end
